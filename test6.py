21de83d600f72362ca31b08a4493e8256fd7c761
Rob McMullen <robm@users.sourceforge.net>
2005-10-30 14:25:11 +0000

    Test for deleting objects to make sure that CreateObject reuses old handles properly.

---
+++ test6.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import pyemf
+
+# Test of handles and deleting objects.
+
+width=8
+height=6
+dpi=100
+
+emf=pyemf.EMF(width,height,dpi)
+pen=[]
+for i in range(16):
+    pen.append(emf.CreatePen(pyemf.PS_SOLID,i,(i*16,0x20,0x40)))
+
+for i in range(16):
+    emf.SelectObject(pen[i])
+    x1=0
+    x2=width*dpi/2
+    y=i*32
+    emf.Polyline([(x1,y),(x2,y)])
+
+emf.DeleteObject(pen[4])
+emf.DeleteObject(pen[10])
+emf.DeleteObject(pen[2])
+emf.DeleteObject(pen[8])
+emf.DeleteObject(pen[15])
+for i in range(16):
+    pen.append(emf.CreatePen(pyemf.PS_DASH,8,(0x01,(15-i)*16,0x03)))
+
+for i in range(16):
+    emf.SelectObject(pen[i])
+    x1=width*dpi/2
+    x2=width*dpi
+    y=i*32
+    emf.Polyline([(x1,y),(x2,y)])
+
+
+emf.save("test6.emf")
